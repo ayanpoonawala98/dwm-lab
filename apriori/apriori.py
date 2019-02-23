c95114ddb38b6f94b9da6d7be22babce7064d8d7
ayanpoonawala98 <rayankhan9822@gmail.com>
2019-02-23 10:00:30 +0530

    Experiment Updated

--- apriori/apriori.py
+++ apriori/apriori.py
@@ -7,30 +7,34 @@ Created on sat Jan 23 08:47:55 2019
 """
 
 #importing the library
-import numpy as np
-import matplotlib as plt
-import pandas as pd
+import numpy as np     #maths function
+import matplotlib as plt #plotting diagram
+import pandas as pd   #file manage
+
 
 # importimg  the data set
-dataset = pd.read_csv('/home/aiktc/Downloads/500_Person_Gender_Height_Weight_Index_for_our_project.csv')
-transactions = []
-for i in range(100000):
-        transactions.append([str(dataset.values[i,j]) for j in range(0,20)])
+#read file help of pandas
+dataset = pd.read_csv('Market_Basket_Optimisation.csv',header=None) 
+transactions = [] #list
+for i in range(0,7501): #range start from 0 to  max
+        transactions.append([str(dataset.values[i,j]) for j in range(0,20)]) #list comprehension
 #Training apyori on the dataset
 from apyori import apriori
-rules = apriori(transactions, min_support = 0.003,min_confidence = 0.2, min_lift = 3, min_length = 2)
+rules = apriori(transactions, min_support = 0.003,min_confidence = 0.2, min_lift = 3, min_length = 2) #apiori aldo
 
 #visualizing results
 
-results = list(rules)
+results = list(rules)#convert in list
 results_list = []
 for i in range(1,len(results)):
-    results_list.append('RULE:\n' + str (results[i][0]).replace('frozenset','') +
-    'n\SUPPORT:\n'+ str (results[i][1]) + 
-    'n\CONF:\n' + str (results[i][2][0][2]) + 
-    'n\LIFT:\n' + str (results[i][0]))
+    results_list.append('RULE:\t' + str (results[i][0]).replace('frozenset','') +
+    'n\SUPPORT:\t' + str (results[i][1]) +
+    'n\CONF:\t' + str (results[i][2][0][2]) + 
+    'n\LIFT:\t' + str (results[i][2][0][3]))
 
-    
-
-    
+''' 
+support(X)=total no of transc/total
+#lift=support(x union y)/support(x)* support(y)
+if x>y
+'''
     
--- clustering/Kmeans_clustering.py
+++ clustering/Kmeans_clustering.py
@@ -18,12 +18,12 @@ X = dataset.iloc[:,[3,4]].values
 
 #using the below method to find the optimal number of clustetrs
 
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans 
 wcss = []
 for i in range(1,11):
-    kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300 """for one cluster it will run 300 times""", n_init = 10, random_state = 0) #
     kmeans.fit(X)
-    wcss.append(kmeans.inertia_)
+    wcss.append(kmeans.inertia_) 
 
 plt.plot(range(1,11),wcss)
 plt.title('The Elbow Method')
@@ -38,7 +38,7 @@ y_kmeans = kmeans.fit_predict(X)
 
 #visualising the clusters
 
-plt.scatter(X[y_kmeans == 0,0],X[y_kmeans == 0,1], s=100,c='red',label='Careful')
+plt.scatter(X[y_kmeans == 0,0],X[y_kmeans == 0,1], s=100,c='red',label='Careful') #scatter mean dotted plot
 plt.scatter(X[y_kmeans == 1,0],X[y_kmeans == 1,1], s=100,c='blue',label='Standard')
 plt.scatter(X[y_kmeans == 2,0],X[y_kmeans == 2,1], s=100,c='green',label='Target')
 plt.scatter(X[y_kmeans == 3,0],X[y_kmeans == 3,1], s=100,c='cyan',label='Careless')
@@ -51,3 +51,5 @@ plt.xlabel('Annual income (k$)')
 plt.ylabel('Spending score (1-100)')
 plt.legend()
 plt.show()
+
+"""max_iter = 300 for one cluster it will run 300 times"""
